05610141d58aa410158d7120e86510e2c21923b2
minaz99 <hanymina43@gmail.com>
2022-11-22 02:20:53 +0100

    Initial deployment- hello world

---
+++ Backend/src/main/java/com/alzproject/alzproject/AlzprojectApplication.java
@@ -0,0 +1,13 @@
+package com.alzproject.alzproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlzprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlzprojectApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/alzproject/alzproject/AlzprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alzproject.alzproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlzprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
